7f7394b09f5292e96fe4610597c03ca336ce1fc5
Lillian Vanderbloemen <lillvander18@gmail.com>
2024-04-06 17:17:59 -0600

    updated the server file to include Apollo

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,3 +1,8 @@
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth')
+
+
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
@@ -15,6 +20,18 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.use(routes);
+
+// Appollo 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers, 
+  context: ({req}) => {
+    const token = req.headers.authorization || '';
+    return { token };
+  },
+  plugins: [authMiddleware],
+});
+server.applyMiddleware({app})
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
